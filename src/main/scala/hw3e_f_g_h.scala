19ca58d79a92723b94483cbcf5259dfba53613b4
vorontsovdg <vwwv@i.ua>
2022-11-28 22:20:54 +0700

    add task 3i

--- src/main/scala/hw3e_f_g_h.scala
+++ src/main/scala/hw3e_f_g_h.scala
@@ -5,12 +5,13 @@ import scala.io.StdIn
  Также в вашу команду пришли два специалиста с окладами 350 и 90 тысяч рублей.
  Попробуйте отсортировать список сотрудников по уровню оклада от меньшего к большему.
  */
-object hw3e_f_g {
+object hw3e_f_g_h {
   def main(args: Array[String]): Unit = {
     var listOfSalaries = ListBuffer(100000f, 150000f, 200000f, 80000f, 120000f, 75000f)
     listOfSalaries.addAll(List(350000f, 90000f))
     //Сортируем и выводим все зарплаты по возрастанию
     listOfSalaries = listOfSalaries.sorted
+    println("Отсортированные по возрастанию зарплаты")
     for (i <- listOfSalaries) {
       println(i)
     }
@@ -28,7 +29,7 @@ object hw3e_f_g {
       }
     }
     //выводим отсортированный список с добавленной зп
-    println(listOfSalaries)
+    println(s"Отсортированные список с добавленной ЗП: $listOfSalaries")
     /*
     Попробуйте вывести номера сотрудников из полученного списка, которые попадают под категорию middle.
     На входе программе подается «вилка» зарплаты специалистов уровня middle.
@@ -37,11 +38,25 @@ object hw3e_f_g {
     val low = StdIn.readFloat()
     print("Введите верхнюю границу ЗП для миддла ")
     val high = StdIn.readFloat()
+    println("Индексы сотрудников, подходящих по ЗП")
     for (i <- 0 until listOfSalaries.length) {
       if (listOfSalaries(i) >= low && listOfSalaries(i) <= high) {
         println(i)
       }
     }
 
+    /*
+    Однако наступил кризис и ваши сотрудники требуют повысить зарплату.
+    Вам необходимо проиндексировать зарплату каждого сотрудника на уровень инфляции – 7%
+     */
+    val salaryAfterRise = listOfSalaries.map(x => x + x * 0.07)
+    println(s"Зарплаты после повышения на 7%: $salaryAfterRise")
+
+
+
+
+
   }
 }
+
+
---
+++ src/main/scala/hw3o.scala
@@ -0,0 +1,42 @@
+import java.text.Bidi
+import scala.annotation.tailrec
+object hw3o {
+  def main(args: Array[String]): Unit = {
+    //обычная рекурсия
+    def powerOfTwo(n: Int): BigInt = {
+      if (n==0) {
+        1
+      }
+      else {
+        if (n==1) {
+          2
+        }
+        else {
+          2 * powerOfTwo(n-1)
+        }
+      }
+    }
+
+
+    def powerOfTwoTailrec(n: Int): BigInt = {
+      @tailrec
+      def loop(x: Int, acc:BigInt = 2): BigInt = {
+        if (x==0) {
+          1
+        }
+        else {
+          if (x == 1) {
+            acc
+          }
+          else {
+            loop(x - 1, acc * 2)
+          }
+        }
+      }
+      loop(n)
+    }
+
+
+    println(powerOfTwoTailrec(0))
+  }
+}
